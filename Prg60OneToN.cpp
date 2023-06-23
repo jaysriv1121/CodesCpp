65a4d8b6d3599c5640f050d7e6e67f3e7bf9f7c2
Jay Srivastava <123503503+jaysriv1121@users.noreply.github.com>
2023-06-23 21:09:21 +0530

    Add files via upload

---
+++ Prg60OneToN.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+void tillN(int n){
+    int x = 1;
+    while(x<n){
+            //x++;
+            cout << x<< endl;
+            x++;
+        }
+    }
+int main(){
+    tillN(59);
+}
