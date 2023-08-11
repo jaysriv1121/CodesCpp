bfc5c836f96540ea8c4b34667106cd237ff7b659
JAY <jaysrivastava1121@gmail.com>
2023-08-11 23:56:49 +0530

    adding cpp file

---
+++ DSA/Length_ofString.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int lengthOfString(char arr[])
+{
+        int counter = 0;
+        for(int i=0; arr[i] !=0; i++){
+                counter++;
+        }
+        cout <<counter<< endl;
+}
+int main()
+{
+    char arr[10];
+    cout << "Enter name"<< endl;
+    cin >> arr;
+    lengthOfString(arr);
+}
